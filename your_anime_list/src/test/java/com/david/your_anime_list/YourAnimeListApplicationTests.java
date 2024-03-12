53826febe9e24654b3119df10a217e025f186b33
David Seu 916 <david.seu@stud.ubbcluj.ro>
2024-03-12 12:17:15 +0200

    Initial commit

---
+++ your_anime_list/src/test/java/com/david/your_anime_list/YourAnimeListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.your_anime_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourAnimeListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
